23e4c08ae4bd8511cbf9ee8f32c498bd01eb98ad
jisha thomas <jishathomas88@gmail.com>
2023-04-24 15:40:39 +0400

    version 5

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log("config.js");
 console.log("version 3")
 console.log("version4")
+console.log("version 5")
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log("index.js")
 console.log("version 2")
 console.log("version 4")
+console.log("version 5")
